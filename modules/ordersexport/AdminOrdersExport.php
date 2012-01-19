0e9875b55ffeb2ff04a1721ac836f34a8cc1a643
Pierrick CAEN <prcaen@gmail.com>
2012-01-19 22:06:04 +0100

    Export fournisseur et transporteur.

--- modules/ordersexport/AdminOrdersExport.php
+++ modules/ordersexport/AdminOrdersExport.php
@@ -347,14 +347,14 @@ class AdminOrdersExport extends AdminTab
       }
       elseif(Tools::getValue('export_type') == 'supplier')
       {
-		    $results = $this->_getResultSupplier($dateStart, $dateEnd, (int)Tools::getValue('order_state'));
-		    $titles  = $this->_getTitlesAdmin();
+		    $results = $this->_getResultSupplier($dateStart, $dateEnd);
+		    $titles  = $this->_getTitlesSupplier($results);
 		    $type    = 'supplier';
       }
       elseif(Tools::getValue('export_type') == 'carrier')
       {
 		    $results = $this->_getResultCarrier($dateStart, $dateEnd, (int)Tools::getValue('order_state'));
-		    $titles  = $this->_getTitlesAdmin();
+		    $titles  = $this->_getTitlesCarrier($results);
 		    $type    = 'carrier';
       }
       else
@@ -453,6 +453,7 @@ class AdminOrdersExport extends AdminTab
                    (SELECT COUNT(oddd.`id_order_detail`) FROM `" . _DB_PREFIX_ . "order_detail` oddd WHERE oddd.`id_order_detail` >= od.`id_order_detail` AND oddd.`id_order` = od.`id_order`) AS `nb_product`,
                    (SELECT COUNT(odd.`id_order`) FROM `" . _DB_PREFIX_ . "order_detail` odd WHERE odd.`id_order` = od.`id_order`) AS `nb_total`,
                    o.`date_add` AS `o.date_add`, 
+                   od.`product_reference` AS `od.product_reference`,
                    m.`name` AS `m.name`, 
                    od.`product_name` AS `od.product_name`, 
                    (SELECT GROUP_CONCAT(agl.`id_attribute_group` ORDER BY agl.`id_attribute_group` ASC SEPARATOR '#')
@@ -467,6 +468,7 @@ class AdminOrdersExport extends AdminTab
                     LEFT JOIN `" . _DB_PREFIX_ . "attribute` a ON (a.`id_attribute` = pac.`id_attribute`) 
                     LEFT JOIN `" . _DB_PREFIX_ . "attribute_lang` al ON (al.`id_attribute` = a.`id_attribute` AND al.`id_lang` = " . (int)$cookie->id_lang . ") 
                     WHERE pa.`id_product_attribute` = od.`product_attribute_id`) AS `al.name`,
+                   od.`product_weight` AS `od.product_weight`,
                    od.`product_quantity` AS `od.product_quantity`, 
                    od.`product_price` AS `et_unit_cost`, 
                    (od.`product_price` * ((100 + (od.`tax_rate`))/100)) AS `ati_unit_cost`, 
@@ -516,6 +518,8 @@ class AdminOrdersExport extends AdminTab
           $datas[$n][$key] = Tools::displayDate($val, (int)$cookie->id_lang, false);
         elseif($key == 'nb_product')
           $datas[$n]['position'] = $val . '/' . $results[$n]['nb_total'];
+        elseif($key == 'od.product_weight')
+          $datas[$n][$key] =  $val . Configuration::get('PS_WEIGHT_UNIT');
         elseif($key == 'osl.name')
         {
           if($result['nb_product'] == 1)
@@ -555,6 +559,7 @@ class AdminOrdersExport extends AdminTab
                    (SELECT COUNT(oddd.`id_order_detail`) FROM `" . _DB_PREFIX_ . "order_detail` oddd WHERE oddd.`id_order_detail` >= od.`id_order_detail` AND oddd.`id_order` = od.`id_order`) AS `nb_product`,
                    (SELECT COUNT(odd.`id_order`) FROM `" . _DB_PREFIX_ . "order_detail` odd WHERE odd.`id_order` = od.`id_order`) AS `nb_total`,
                    o.`date_add` AS `o.date_add`, 
+                   od.`product_reference` AS `od.product_reference`,
                    m.`name` AS `m.name`, 
                    od.`product_name` AS `od.product_name`, 
                    (SELECT GROUP_CONCAT(agl.`id_attribute_group` ORDER BY agl.`id_attribute_group` ASC SEPARATOR '#')
@@ -569,6 +574,7 @@ class AdminOrdersExport extends AdminTab
                     LEFT JOIN `" . _DB_PREFIX_ . "attribute` a ON (a.`id_attribute` = pac.`id_attribute`) 
                     LEFT JOIN `" . _DB_PREFIX_ . "attribute_lang` al ON (al.`id_attribute` = a.`id_attribute` AND al.`id_lang` = " . (int)$cookie->id_lang . ") 
                     WHERE pa.`id_product_attribute` = od.`product_attribute_id`) AS `al.name`,
+                   od.`product_weight` AS `od.product_weight`,
                    od.`product_quantity` AS `od.product_quantity`, 
                    od.`product_price` AS `et_unit_cost`, 
                    (od.`product_price` * ((100 + (od.`tax_rate`))/100)) AS `ati_unit_cost`, 
@@ -586,8 +592,7 @@ class AdminOrdersExport extends AdminTab
                    adi.`postcode` AS `invoice_postcode`, 
                    adi.`city` AS `invoice_city`, 
                    cli.`name` AS `invoice_country`, 
-                   u.`email` AS `u.email`,
-                   osl.`name` AS `osl.name`
+                   u.`email` AS `u.email`
               FROM `" . _DB_PREFIX_ . "orders` o
               LEFT JOIN `" . _DB_PREFIX_ . "order_detail` od ON (od.`id_order` = o.`id_order`)
               LEFT JOIN `" . _DB_PREFIX_ . "product` p ON (p.`id_product` = od.`product_id`) 
@@ -618,6 +623,8 @@ class AdminOrdersExport extends AdminTab
           $datas[$n][$key] = Tools::displayDate($val, (int)$cookie->id_lang, false);
         elseif($key == 'nb_product')
           $datas[$n]['position'] = $val . '/' . $results[$n]['nb_total'];
+        elseif($key == 'od.product_weight')
+          $datas[$n][$key] =  $val . Configuration::get('PS_WEIGHT_UNIT');
         elseif($key == 'osl.name')
         {
           if($result['nb_product'] == 1)
@@ -645,68 +652,35 @@ class AdminOrdersExport extends AdminTab
     return $datas;
   }
   
-  private function _getResultSupplier($dateStart, $dateEnd, $orderState)
+  private function _getResultSupplier($dateStart, $dateEnd)
   {
     global $cookie;
 
-    if($orderState != 0)
-      $state = " AND oh.`id_order_state` = " . $orderState;
-    else
-      $state = "";
-    $sql = "SELECT SQL_CALC_FOUND_ROWS o.`id_order` AS `o.id_order`, 
-                   (SELECT COUNT(oddd.`id_order_detail`) FROM `" . _DB_PREFIX_ . "order_detail` oddd WHERE oddd.`id_order_detail` >= od.`id_order_detail` AND oddd.`id_order` = od.`id_order`) AS `nb_product`,
-                   (SELECT COUNT(odd.`id_order`) FROM `" . _DB_PREFIX_ . "order_detail` odd WHERE odd.`id_order` = od.`id_order`) AS `nb_total`,
-                   o.`date_add` AS `o.date_add`, 
-                   m.`name` AS `m.name`, 
-                   od.`product_name` AS `od.product_name`, 
-                   (SELECT GROUP_CONCAT(agl.`id_attribute_group` ORDER BY agl.`id_attribute_group` ASC SEPARATOR '#')
-                    FROM `" . _DB_PREFIX_ . "product_attribute` pa
-                    LEFT JOIN `" . _DB_PREFIX_ . "product_attribute_combination` pac ON (pac.`id_product_attribute` = pa.`id_product_attribute`) 
-                    LEFT JOIN `" . _DB_PREFIX_ . "attribute` a ON (a.`id_attribute` = pac.`id_attribute`) 
-                    LEFT JOIN `" . _DB_PREFIX_ . "attribute_group_lang` agl ON (agl.`id_attribute_group` = a.`id_attribute_group` AND agl.`id_lang` = " . (int)$cookie->id_lang . ")
-                    WHERE pa.`id_product_attribute` = od.`product_attribute_id`) AS `agl.id_attribute_group`,
-                   (SELECT GROUP_CONCAT(al.`name` ORDER BY al.`name` ASC SEPARATOR '#')
-                    FROM `" . _DB_PREFIX_ . "product_attribute` pa
-                    LEFT JOIN `" . _DB_PREFIX_ . "product_attribute_combination` pac ON (pac.`id_product_attribute` = pa.`id_product_attribute`) 
-                    LEFT JOIN `" . _DB_PREFIX_ . "attribute` a ON (a.`id_attribute` = pac.`id_attribute`) 
-                    LEFT JOIN `" . _DB_PREFIX_ . "attribute_lang` al ON (al.`id_attribute` = a.`id_attribute` AND al.`id_lang` = " . (int)$cookie->id_lang . ") 
-                    WHERE pa.`id_product_attribute` = od.`product_attribute_id`) AS `al.name`,
-                   od.`product_quantity` AS `od.product_quantity`, 
-                   od.`product_price` AS `et_unit_cost`, 
-                   (od.`product_price` * ((100 + (od.`tax_rate`))/100)) AS `ati_unit_cost`, 
-                   (od.`product_price` * od.`product_quantity`) AS `et_cost`, 
-                   (od.`product_price` * ((100 + (od.`tax_rate`))/100) * od.`product_quantity`) AS `ati_cost`, 
-                   ads.`lastname` AS `delivery_lastname`, 
-                   ads.`firstname` AS `delivery_firstname`, 
-                   CONCAT(ads.`address1`, ' ', ads.`address2`) AS `delivery_address`, 
-                   ads.`postcode` AS `delivery_postcode`, 
-                   ads.`city` AS `delivery_city`, 
-                   cld.`name` AS `delivery_country`, 
-                   adi.`lastname` AS `invoice_lastname`, 
-                   adi.`firstname` AS `invoice_firstname`, 
-                   CONCAT(adi.`address1`, ' ', adi.`address2`) AS `invoice_address`, 
-                   adi.`postcode` AS `invoice_postcode`, 
-                   adi.`city` AS `invoice_city`, 
-                   cli.`name` AS `invoice_country`, 
-                   u.`email` AS `u.email`,
-                   osl.`name` AS `osl.name`
+    $sql = "SELECT SQL_CALC_FOUND_ROWS
+                    o.`date_add` AS `o.date_add`, 
+                    m.`name` AS `m.name`,
+                    od.`product_reference` AS `od.product_reference`,
+                    od.`product_name` AS `od.product_name`, 
+                    (SELECT GROUP_CONCAT(agl.`id_attribute_group` ORDER BY agl.`id_attribute_group` ASC SEPARATOR '#')
+                      FROM `" . _DB_PREFIX_ . "product_attribute` pa
+                      LEFT JOIN `" . _DB_PREFIX_ . "product_attribute_combination` pac ON (pac.`id_product_attribute` = pa.`id_product_attribute`) 
+                      LEFT JOIN `" . _DB_PREFIX_ . "attribute` a ON (a.`id_attribute` = pac.`id_attribute`) 
+                      LEFT JOIN `" . _DB_PREFIX_ . "attribute_group_lang` agl ON (agl.`id_attribute_group` = a.`id_attribute_group` AND agl.`id_lang` = " . (int)$cookie->id_lang . ")
+                      WHERE pa.`id_product_attribute` = od.`product_attribute_id`) AS `agl.id_attribute_group`,
+                     (SELECT GROUP_CONCAT(al.`name` ORDER BY al.`name` ASC SEPARATOR '#')
+                      FROM `" . _DB_PREFIX_ . "product_attribute` pa
+                      LEFT JOIN `" . _DB_PREFIX_ . "product_attribute_combination` pac ON (pac.`id_product_attribute` = pa.`id_product_attribute`) 
+                      LEFT JOIN `" . _DB_PREFIX_ . "attribute` a ON (a.`id_attribute` = pac.`id_attribute`) 
+                      LEFT JOIN `" . _DB_PREFIX_ . "attribute_lang` al ON (al.`id_attribute` = a.`id_attribute` AND al.`id_lang` = " . (int)$cookie->id_lang . ") 
+                      WHERE pa.`id_product_attribute` = od.`product_attribute_id`) AS `al.name`,
+                      SUM(od.`product_quantity`) AS `quantity`
               FROM `" . _DB_PREFIX_ . "orders` o
               LEFT JOIN `" . _DB_PREFIX_ . "order_detail` od ON (od.`id_order` = o.`id_order`)
               LEFT JOIN `" . _DB_PREFIX_ . "product` p ON (p.`id_product` = od.`product_id`) 
-              LEFT JOIN `" . _DB_PREFIX_ . "manufacturer` m ON (m.`id_manufacturer` = p.`id_manufacturer`) 
-              LEFT JOIN `" . _DB_PREFIX_ . "customer` u ON (u.`id_customer` = o.`id_customer`) 
-              LEFT JOIN `" . _DB_PREFIX_ . "address` ads ON (ads.`id_address` = o.`id_address_delivery`) 
-              LEFT JOIN `" . _DB_PREFIX_ . "address` adi ON (adi.`id_address` = o.`id_address_invoice`) 
-              LEFT JOIN `" . _DB_PREFIX_ . "country_lang` cli ON (cli.`id_country` = adi.`id_country` AND cli.`id_lang` = " . (int)$cookie->id_lang . ") 
-              LEFT JOIN `" . _DB_PREFIX_ . "country_lang` cld ON (cld.`id_country` = ads.`id_country` AND cld.`id_lang` = " . (int)$cookie->id_lang . ") 
-              
-              LEFT JOIN `" . _DB_PREFIX_ . "tax_rule` tr ON (p.`id_tax_rules_group` = tr.`id_tax_rules_group` AND tr.`id_country` = " . (int)Country::getDefaultCountryId() . " AND tr.`id_state` = 0) 
-              LEFT JOIN `" . _DB_PREFIX_ . "tax` t ON (t.`id_tax` = tr.`id_tax`)
-              LEFT JOIN `" . _DB_PREFIX_ . "order_history` oh ON (oh.`id_order` = o.`id_order`)
-              LEFT JOIN `" . _DB_PREFIX_ . "order_state` os ON (os.`id_order_state` = oh.`id_order_state`)
-              LEFT JOIN `" . _DB_PREFIX_ . "order_state_lang` osl ON (osl.`id_order_state` = oh.`id_order_state` AND osl.`id_lang` = " . (int)$cookie->id_lang . ") 
-              WHERE o.date_add BETWEEN '" . $dateStart . "' AND '" . $dateEnd . "'" . $state . "
-              ORDER BY o.`id_order` ASC, nb_product ASC";
+              LEFT JOIN `" . _DB_PREFIX_ . "manufacturer` m ON (m.`id_manufacturer` = p.`id_manufacturer`)
+              WHERE o.date_add BETWEEN '" . $dateStart . "' AND '" . $dateEnd . "'
+              GROUP BY od.`product_attribute_id`, od.`product_id`";
+
     $results = Db::getInstance()->ExecuteS($sql);
     $datas   = array();
 
@@ -714,17 +688,8 @@ class AdminOrdersExport extends AdminTab
     {
       foreach($result as $key => $val)
       {
-        if($key == 'et_unit_cost' || $key == 'ati_unit_cost' || $key == 'et_cost' || $key == 'ati_cost')
-          $datas[$n][$key] = Tools::displayPrice($val, $this->currency, false);
-        elseif($key == 'o.date_add')
+        if($key == 'o.date_add')
           $datas[$n][$key] = Tools::displayDate($val, (int)$cookie->id_lang, false);
-        elseif($key == 'nb_product')
-          $datas[$n]['position'] = $val . '/' . $results[$n]['nb_total'];
-        elseif($key == 'osl.name')
-        {
-          if($result['nb_product'] == 1)
-            $datas[$n][$key] = $val;
-        }
         elseif($key == 'al.name')
         {
           $datas[$n] = $this->_getFeatureArray($datas[$n]);
@@ -737,7 +702,7 @@ class AdminOrdersExport extends AdminTab
               $datas[$n]['feature_' . $featuresIds[$k]] = $feature;
           }
         }
-        elseif($key == 'nb_total' || $key == 'agl.id_attribute_group')
+        elseif($key == 'agl.id_attribute_group')
           continue;
         else
           $datas[$n][$key] = $val;
@@ -753,6 +718,7 @@ class AdminOrdersExport extends AdminTab
       $this->l('Order reference'),
       $this->l('Product n°/Total products'),
       $this->l('Date'),
+      $this->l('Product reference'),
       $this->l('Manufacturer'),
       $this->l('Product name')
     );
@@ -761,6 +727,7 @@ class AdminOrdersExport extends AdminTab
         $titles[] = $att['name'];
 
     array_push($titles,
+      $this->l('Weight'),
       $this->l('Quantity'),
       $this->l('ET unit cost') . ' (' . $this->_currencySign . ')',
       $this->l('ATI unit cost') . ' (' . $this->_currencySign . ')',
@@ -784,6 +751,64 @@ class AdminOrdersExport extends AdminTab
     return $titles;
   }
   
+  private function _getTitlesCarrier($results)
+  {
+    $titles = array(
+      $this->l('Order reference'),
+      $this->l('Product n°/Total products'),
+      $this->l('Date'),
+      $this->l('Product reference'),
+      $this->l('Manufacturer'),
+      $this->l('Product name')
+    );
+
+    foreach($this->_getGroupAttributes() AS $att)
+        $titles[] = $att['name'];
+
+    array_push($titles,
+      $this->l('Weight'),
+      $this->l('Quantity'),
+      $this->l('ET unit cost') . ' (' . $this->_currencySign . ')',
+      $this->l('ATI unit cost') . ' (' . $this->_currencySign . ')',
+      $this->l('ET cost') . ' (' . $this->_currencySign . ')',
+      $this->l('ATI cost') . ' (' . $this->_currencySign . ')',
+      $this->l('Delivery lastname'),
+      $this->l('Delivery firstname'),
+      $this->l('Delivery address'),
+      $this->l('Delivery zip code'),
+      $this->l('Delivery city'),
+      $this->l('Delivery country'),
+      $this->l('Invoice lastname'),
+      $this->l('Invoice firstname'),
+      $this->l('Invoice address'),
+      $this->l('Invoice zip code'),
+      $this->l('Invoice city'),
+      $this->l('Invoice country'),
+      $this->l('Email')
+    );
+    
+    return $titles;
+  }
+  
+  private function _getTitlesSupplier($results)
+  {
+    $titles = array(
+      $this->l('Date'),
+      $this->l('Manufacturer'),
+      $this->l('Product reference'),
+      $this->l('Product name')
+    );
+
+    foreach($this->_getGroupAttributes() AS $att)
+        $titles[] = $att['name'];
+
+    array_push($titles,
+      $this->l('Quantity')
+    );
+    
+    return $titles;
+  }
+
   private function _getFeatureArray($array)
   {
     for ($i=1; $i <= count($this->_getGroupAttributes()); $i++) { 
